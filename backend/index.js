348655c39e17e9a6c4e74ad9a679e25a82a1086a
micuami <anamihaela.micu@gmail.com>
2023-11-27 19:39:02 +0200

    Created database connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express= require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/probaIT');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
+db.once('open', () => console.log('Connected to MongoDB'));
+const MyModel = mongoose.model('Test', new mongoose.Schema({ name: String }));
+const test = new MyModel({name: "name"});
+test.save();
+
 app.get("/", (req, resp)=>{
-    resp.send("app is working...")
+    resp.send("app is working...");
 });
 
-app.listen(5000)
+app.listen(5000);
